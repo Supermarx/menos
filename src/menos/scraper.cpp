b4cc6e628b0725b10deb5c40cf91fb2feaa6f461
Wouter Geraedts <git@woutergeraedts.nl>
2015-06-23 18:11:32 +0200

    Changes from upstream restructure

--- src/menos/scraper.cpp
+++ src/menos/scraper.cpp
@@ -46,7 +46,7 @@ namespace supermarx
 			todo.pop();
 
 			size_t product_i = 0;
-			product_parser pp([&](const product& p, boost::optional<std::string> const& _image_uri, datetime retrieved_on, confidence conf, problems_t probs)
+			product_parser pp([&](const message::product_base& p, boost::optional<std::string> const& _image_uri, datetime retrieved_on, confidence conf, problems_t probs)
 			{
 				++product_i;
 
